05bb128206b1ae809605c254a4c3f0537456df35
leandrosouzaxd <31319438+leandrosouzaxd@users.noreply.github.com>
2018-01-26 02:50:15 -0200

    arquivo
    inserindo arquivo py correto

---
+++ Sintegra/ConsultarCnpj.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from bs4 import *
+import requests
+from urllib import parse
+
+URL = 'http://www.sefaz.ce.gov.br/content/aplicacao/internet/servicos_online/sintegra/sintegra.asp?estado=ce'
+s = requests.Session()
+
+
+def fetch(url, data=None):
+    if data is None:
+        return s.get(url).content
+    else:
+        return s.post(url, data=data).content
+
+
+soup = BeautifulSoup(fetch(URL), "html.parser")
+form = soup.find('form')
+fields = form.findAll('input')
+
+formdata = dict((field.get('name'), field.get('value')) for field in fields)
+
+#input
+input_CNPJ = str(input("insira o cnpj: "))#'07623077000167'
+while len(input_CNPJ) != 14:
+        print('Verifique o CNPJ digitado!')
+        input_CNPJ = str(input("insira o cnpj: "))
+
+formdata['cmbCampo'] = 'CNPJ'
+formdata['txtValor'] = input_CNPJ
+formdata['B1'] = 'Consultar'
+
+
+#url inicial de pesquisa + form preenchido
+posturl = parse.urljoin(URL, form['action'])
+
+
+r = s.post(posturl, data=formdata)
+resultado = r.content
+
+
+soup_resultado = BeautifulSoup(resultado, "html.parser")
+
+
+lista_td = soup_resultado.find('body')
+
+
+dados = []
+
+
+for texto in lista_td.find_all('td',{'bgcolor': "#FAFAE4"}):
+    dados[0] = dados.append(texto.text.replace('\xa0', ''))
+
+cnpj = input_CNPJ
+
+
+#inicio replaces I E
+ie = dados[1].replace('.','')
+ie = ie.replace('-','')
+ie = ie.replace(' ','')
+#fim
+
+razao = dados[2]
+ender = dados[3]
+num = dados[4]
+bairro = dados[5]
+cidade = dados[6]
+
+# dados[7] é o estado por extenso
+estado = 'CE'
+
+# inicio replaces cep para tirar os pontos
+cep = dados[9].replace('.','')
+cep = cep.replace('-','')
+cep = cep.replace(' ','')
+#fim
+telefone = dados[10]
+situacao = dados[13]
+
+print(cnpj)
+print(ie)
+print(razao)
+print(ender)
+print(num)
+print(bairro)
+print(cidade)
+print(cep)
+print(telefone)
+
+print(dados)
+
+
