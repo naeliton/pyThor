966a3ccf0c1db32fd2fbc4bec6d12eee16cea4ac
leandrosouzaxd <31319438+leandrosouzaxd@users.noreply.github.com>
2018-01-26 01:56:28 -0200

    excluindo uns prints
    apagando umas linhas desnecessárias

--- geolocalização/GeoUpdate.py
+++ geolocalização/GeoUpdate.py
@@ -2,17 +2,15 @@ import cx_Oracle
 from pyThorGeo.geolocalização import geoPEsquisa
 from pyThorGeo.geolocalização import geopyWin
 
-TNS = cx_Oracle.makedsn("192.168.0.3", "1521", "WINT")
+TNS = cx_Oracle.makedsn("localhost", "1521", "orcl")
 con = cx_Oracle.connect(user="dsl", password="dsl", dsn=TNS)
-print('validando informaçoes')
 latitude = geoPEsquisa.latitude
 longitude = geoPEsquisa.longitude
 codcli = geopyWin.Pesquisa.codcli
 
 cur = con.cursor()
 statement = 'update pcclient_bkp set latitude = :latitude, longitude =:longitude where codcli = :codcli'
-print('Aplicando atualização')
-cur.execute(statement, (latitude,longitude, codcli))
+cur.execute(statement, (latitude, longitude, codcli))
 con.commit()
 cur.close()
 con.close()
